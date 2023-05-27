2f5faca725f55156bb29d26597968dfa9952d865
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2023-05-26 22:20:47 -0600

    String dictionary from AppleScript response

--- Apple Notes Exporter/Apple Notes Exporter/ContentView.swift
+++ Apple Notes Exporter/Apple Notes Exporter/ContentView.swift
@@ -24,6 +24,66 @@ extension NSAppleEventDescriptor {
         return (0..<listDescriptor.numberOfItems)
             .compactMap { listDescriptor.atIndex($0 + 1)?.stringValue }
     }
+    
+    func toStringDictionary() -> [String : String] {
+        guard let listDescriptor = self.coerce(toDescriptorType: typeAERecord) else {
+            return [:]
+        }
+        
+        print(listDescriptor)
+        
+        return [:]
+        
+        //return (0..<listDescriptor.numberOfItems).compactMap { listDescriptor.atIndex($0 + 1)?.stringValue }
+    }
+    
+        /*func convertToSwiftObject(descriptor: NSAppleEventDescriptor) -> Any? {
+        switch descriptor.descriptorType {
+        case .nullDescriptor:
+            return nil
+        case .booleanDescriptor:
+            return descriptor.booleanValue
+        case .typeUTF8Text, .typeUnicodeText:
+            return descriptor.stringValue
+        case .typeInteger:
+            return descriptor.intValue
+        case .typeFloat:
+            return descriptor.floatValue
+        case .typeDouble:
+            return descriptor.doubleValue
+        case .dateDescriptor:
+            return descriptor.dateValue
+        case .descriptorTypeAlias:
+            return descriptor.fileURLValue
+        case .listDescriptor:
+            var swiftArray = [Any]()
+            for i in 1...descriptor.numberOfItems {
+                if let itemDescriptor = descriptor.descriptor(at: i) {
+                    if let item = convertToSwiftObject(itemDescriptor) {
+                        swiftArray.append(item)
+                    }
+                }
+            }
+            return swiftArray
+        case .recordDescriptor:
+            var swiftDictionary = [String: Any]()
+            if let keysDescriptor = descriptor.coerce(toDescriptorType: .typeAEList) {
+                let keysCount = keysDescriptor.numberOfItems
+                for i in 1...keysCount {
+                    if let keyDescriptor = keysDescriptor.descriptor(at: i),
+                       let key = keyDescriptor.stringValue,
+                       let valueDescriptor = descriptor.descriptor(forKeyword: keyDescriptor.typeCodeValue) {
+                        if let value = convertToSwiftObject(valueDescriptor) {
+                            swiftDictionary[key] = value
+                        }
+                    }
+                }
+            }
+            return swiftDictionary
+        default:
+            return descriptor.stringValue
+        }
+    }*/
 }
 
 struct AppleScript {
@@ -45,19 +105,41 @@ struct AppleScript {
         // Return an empty string if no result
         return []
     }
+    
+    func stringDictionary(script: String) -> [String : String] {
+        // Create the new NSAppleScript instance
+        if let scriptObject = NSAppleScript(source: script) {
+            // Error dictionary
+            var errorDict: NSDictionary? = nil
+            // Execute the script, adding to the errorDict if there are errors
+            let resultDescriptor = scriptObject.executeAndReturnError(&errorDict)
+            // If there are no errors, return the resultDescriptor after converting it to a string array
+            if errorDict == nil {
+                return resultDescriptor.toStringDictionary()
+            }
+        }
+        // Return an empty string if no result
+        return [:]
+    }
 }
 
 struct ContentView: View {
-    func getNoteAccounts() {
-        let loadAllScript = """
+    func exportNotes() {
+        // Script to export the notes from the current account
+        let exportScript = """
             set noteList to {}
-            return { "test" }
             tell application "Notes"
+                repeat with theAccount in accounts
+                    if name of theAccount as string = "\(selectedNotesAccount)" then
+                        set chosenAccount to theAccount
+                    end if
+                end repeat
                 repeat with noteFolder in folders
                     repeat with myNote in notes of noteFolder
                         set noteTitle to name of myNote
                         set noteBody to body of myNote
-                        set noteItem to {title:noteTitle, body:noteBody}
+        --body:noteBody,
+                        set noteItem to {title:noteTitle, folder:noteFolder}
                         set end of noteList to noteItem
                     end repeat
                 end repeat
@@ -65,21 +147,7 @@ struct ContentView: View {
             end tell
         """
         
-        let countScript = """
-            set AppleScript's text item delimiters to linefeed
-            set noteList to {}
-            tell application "Notes"
-                repeat with noteFolder in folders
-                    repeat with myNote in notes of noteFolder
-                        set noteTitle to name of myNote
-                        set end of noteList to noteTitle
-                    end repeat
-                end repeat
-            end tell
-            return noteList
-        """
-        
-        AppleScript().stringArray(script: countScript)
+        AppleScript().stringDictionary(script: exportScript)
     }
     
     init() {
@@ -171,7 +239,7 @@ struct ContentView: View {
             
             Text("Step 4: Export!").font(.title).multilineTextAlignment(.leading).lineLimit(1)
             Button(action: {
-                getNoteAccounts()
+                exportNotes()
             }) {
                 Text("Export").frame(maxWidth: .infinity)
             }.buttonStyle(.borderedProminent)
